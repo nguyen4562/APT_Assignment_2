0cb38d8e5ce35dc1dd97e24eba02f06028a8baa1
nguyen4562 <s3819521@rmit.edu.vn>
2021-12-17 19:07:21 +0700

    Nguyen seperate Interface class

---
+++ Interface.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "RentalShop.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Interface {
+private:
+    int menuChoice;
+    RentalShop* rs;
+public:
+    Interface();
+
+    void menu();
+    void menu1();
+    void menu2();
+    void menu3();
+    void menu4();
+    void menu5();
+    void menu6();
+    void menu7();
+    void menu8();
+    void menu9();
+    void menu10();
+
+    void option(int choice, RentalShop** rs);
+    int takeChoice(int limit);
+};
